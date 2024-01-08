e5b6013d12d123595cf0dac6ed45f038feaffba9
urvashiramdasani <urvashi.ramdasani@gmail.com>
2024-01-08 20:35:36 +0530

    Add Hamburger Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 
 // Hooks
@@ -9,11 +9,17 @@ import { Wrapper, Logo } from './Header.styles'
 
 // Components
 import Menu from 'components/Menu'
+import Hamburger from 'components/Hamburger'
+import MobileMenu from 'components/MobileMenu'
 
 const Header = ({ siteTitle = `` }) => {
     const siteConfig = useConfigQuery()
+    const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <Wrapper>
+      <Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} items={siteConfig.menu} />
       <Menu items={siteConfig.menu} />
       <Link to='/'>
         <Logo src={siteConfig.logo.publicURL} alt={siteTitle} />
