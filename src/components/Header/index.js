2ef279a7a16a7355b2ba1c4cf1f3fd43526f8745
urvashiramdasani <urvashi.ramdasani@gmail.com>
2024-01-08 19:51:53 +0530

    Add Menu Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,13 +7,18 @@ import { useConfigQuery } from 'hooks/useConfigQuery'
 // Styles
 import { Wrapper, Logo } from './Header.styles'
 
+// Components
+import Menu from 'components/Menu'
+
 const Header = ({ siteTitle = `` }) => {
     const siteConfig = useConfigQuery()
   return (
     <Wrapper>
+      <Menu items={siteConfig.menu} />
       <Link to='/'>
         <Logo src={siteConfig.logo.publicURL} alt={siteTitle} />
       </Link>
+      <div>Theme Button</div>
     </Wrapper>
   )
 }
